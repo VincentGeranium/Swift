d380b42db40c35fcdad2c4f7b2105fec840fc66f
김광준 <kwangjun3952@gmail.com>
2019-04-18 13:54:39 +0900

    xcode: Study about Unwind of Segue

---
+++ Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Scene-UnwindSegue
+//
+//  Created by 김광준 on 17/04/2019.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue) {
+        
+    }
+}
+
