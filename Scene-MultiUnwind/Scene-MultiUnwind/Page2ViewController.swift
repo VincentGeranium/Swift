f261c7e39b5f4bb47716ec69e9769f9e8165ee7a
김광준 <kwangjun3952@gmail.com>
2019-04-18 20:40:54 +0900

    xcode: Study about two different way of Unwind Action Segue

---
+++ Scene-MultiUnwind/Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 김광준 on 18/04/2019.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+    
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue) {
+        
+    }
+}
---
+++ Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 김광준 on 18/04/2019.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // Unwind를 위해 정의한 메소드. 따라서 스토리보드의 객체와 연결할 필요는 없다.
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
+        
+    }
+
+}
+
